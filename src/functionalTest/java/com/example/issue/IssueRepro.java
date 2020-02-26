91f18eb2239f81f3670409f72486f09832cb7b8e
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2020-02-26 19:59:42 +0100

    Simplify test

--- src/functionalTest/java/com/example/issue/IssueRepro.java
+++ src/functionalTest/java/com/example/issue/IssueRepro.java
@@ -1,21 +1,10 @@
 package com.example.issue;
 
-import org.gradle.testkit.runner.GradleRunner;
 import org.testng.annotations.Test;
-import org.gradle.testkit.runner.BuildResult;
-
-import java.io.File;
 
 public class IssueRepro {
 
     @Test
     public void reproduce() {
-        BuildResult result = GradleRunner.create()
-                .withProjectDir(new File("src/functionalTest/resources"))
-                .withArguments(":clean")
-                .build();
-
-        String output = result.getOutput();
-        System.out.println(output);
     }
 }
